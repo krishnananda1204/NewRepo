cd4b423a0c7d4692f9fde68cae9cd2350176a508
Admin <Admin@DESKTOP-4IG8LJS>
2025-01-01 05:56:20 +0530

    My first project commited

---
+++ Mymaven/src/main/java/string1/StringClass.java
@@ -0,0 +1,41 @@
+package string1;
+
+public class StringClass {
+
+	public static void main(String args[])
+	{
+		
+		StringBuffer sb= new StringBuffer("Hello");
+		System.out.println(sb);
+		
+		StringBuilder sbs= new StringBuilder("World");
+		System.out.println(sbs);
+		
+		//append - to add new string at last
+		sb.append("hi");
+		System.out.println(sb);
+		
+		sbs.append("hi");
+		System.out.println(sbs);
+		
+		//INSERT
+		sb.insert(3, "hey");
+		System.out.println(sb);
+		
+		//replace
+		sb.replace(1, 3, "java");
+		System.out.println(sb);
+		sbs.replace(1, 12, "K");
+		System.out.println(sbs);
+		
+		//REVERSE
+		sb.reverse();
+		System.out.println(sb);
+		
+		//delete
+		sb.delete(0, 4);
+		System.out.println(sb);
+		
+	}
+
+}
